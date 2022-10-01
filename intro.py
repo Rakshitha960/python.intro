9761589d221266ca3a3633b270df52c10d33005e
Rakshitha960 <74966605+Rakshitha960@users.noreply.github.com>
2022-10-01 14:58:20 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+print("Hello world")
+name = input("Enter your name ")
+print("Hello "+ name ) 
+#List are used to store multiple items
+#arrays are called list in python
+friends = ["bob" , "john" , "javani" , "aish"]
+print(len (friends))
+if name == "Rakshitha" : 
+    print("it's me! ")
+    
